e22e3c1cc3eeaafed047965cd7b0b687a51a5b7c
Mason Fabel <fabe0940@vandals.uidaho.edu>
2016-04-07 13:11:24 -0700

    Implemented uik_initialize()
    Created initial implementation of uik_initialize(), which merely
    starts Timer 1 in output compare mode with the requested interrupt
    frequency.
    
    Added defines needed to interface with Timer 1.

--- kernel/defines.h
+++ kernel/defines.h
@@ -1,10 +1,12 @@
 #ifndef _KERNEL_DEFINES_H_
 #define _KERNEL_DEFINES_H_
 
+#include "dispatch.h"
+#include "idle.h"
 #include "kernel.h"
 #include "task.h"
 #include "tick.h"
-#include "dispatch.h"
-#include "idle.h"
+#include "timer.h"
+#include "queue.h"
 
 #endif /* _KERNEL_DEFINES_H_ */
--- kernel/init.c
+++ kernel/init.c
@@ -1,6 +1,24 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
 #include <inttypes.h>
 #include "defines.h"
 
 void uik_initialize(uint16_t tick_len, uint8_t max_tasks) {
+	cli();
+
+	TCCR1A = _UIK_TIMER_MODE_CTC_A | _UIK_TIMER_MODE_NORMAL_PORT;
+	TCCR1B = _UIK_TIMER_MODE_CTC_B | _UIK_TIMER_PRESCALER_1;
+
+	TIMSK |= (0x01 << OCIE1A);
+
+	TCNT1H = 0x00;
+	TCNT1L = 0x00;
+
+	OCR1A = tick_len;
+
+	uik_task_add(_uik_task_idle, _UIK_TASK_PRIORITY_MIN, _UIK_TASK_STACK_MIN);
+
+	sei();
+
 	return;
 }
